d950170d10cb20bd73849aaebaa494f6964d76fb
Ym111 <ym_chen95@outlook.com>
2020-09-21 17:22:48 -0700

    add object pick up

---
+++ Assets/Codes/Collections/CollectionsAction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionsAction : MonoBehaviour
+{
+	
+
+	public 
+	bool pickUpAllowed ;
+	public 
+	Text pickUpText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pickUpAllowed && Input.GetKeyDown(KeyCode.E)){
+        	PickUp();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+    	if(collision.gameObject.name.Equals("Jack")){
+    		pickUpText.gameObject.SetActive(true);
+    		pickUpAllowed = true;
+    	}
+    }
+
+    void OnTriggerExit2D(Collider2D collision){
+    	if(collision.gameObject.name.Equals("Jack")){
+    		pickUpText.gameObject.SetActive(false);
+    		pickUpAllowed = false;
+    	}
+    }
+    
+    void PickUp(){
+    	Destroy(gameObject);
+    }
+}
