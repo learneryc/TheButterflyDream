438dc992d56fabac4797551e2ed17c91ebb3e72a
yuchuliu <yuchuliu@usc.edu>
2020-09-21 21:09:58 -0700

    add menu

---
+++ Assets/Codes/Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public string SceneName="";
+	public Animator transition;
+	public string trigger = "";
+	public float transitionTime = 1f;
+
+	public void PlayGame() {
+		StartCoroutine(LoadLevel());
+	}
+
+	IEnumerator LoadLevel() {
+		if (trigger!="") {
+			transition.SetTrigger(trigger);
+			yield return new WaitForSeconds(transitionTime);
+		}
+		if (SceneName != "") SceneManager.LoadScene(SceneName);
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+    
+}
