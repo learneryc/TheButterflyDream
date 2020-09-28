1fb5d549465fef183435b70479cd92f2e4a5f763
InfiniteEEMS <wendache@usc.edu>
2020-09-28 00:31:16 -0700

    ParallexLayer, move map and camera

---
+++ Assets/Codes/Camera/ParallaxLayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Platformer.View
+{
+    /// <summary>
+    /// Used to move a transform relative to the main camera position with a scale factor applied.
+    /// This is used to implement parallax scrolling effects on different branches of gameobjects.
+    /// </summary>
+    public class ParallaxLayer : MonoBehaviour
+    {
+        /// <summary>
+        /// Movement of the layer is scaled by this value.
+        /// </summary>
+        public Vector3 movementScale = new Vector3(1,0,0);
+
+        Transform _camera;
+
+        void Awake()
+        {
+            _camera = Camera.main.transform;
+        }
+
+        void LateUpdate()
+        {
+            transform.position = Vector3.Scale(_camera.position, movementScale);
+        }
+
+    }
+}
