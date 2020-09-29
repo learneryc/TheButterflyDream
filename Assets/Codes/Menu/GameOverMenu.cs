e762fc2bccb6e799a8018e02fb0cdbe96a0258bb
new-yongsheng <yongshen@usc.edu>
2020-09-28 17:20:19 -0700

    Add Game Over Scence
    Add a Game Over Scence. The Scence have two options. First one is restart curent level, the second one is back to main menu.

---
+++ Assets/Codes/Menu/GameOverMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+	public string SceneName="";
+	public Animator transition;
+	public string trigger = "";
+	public float transitionTime = 1f;
+
+	public void Restart() {
+		StartCoroutine(LoadLevel());
+	}
+
+	IEnumerator LoadLevel() {
+		if (trigger!="") {
+			transition.SetTrigger(trigger);
+			yield return new WaitForSeconds(transitionTime);
+		}
+		if (SceneName != "") SceneManager.LoadScene(SceneName);
+	}
+
+	public void QuitGame() {
+		SceneName="StartMenu";
+		StartCoroutine(LoadLevel());
+	}
+    
+}
