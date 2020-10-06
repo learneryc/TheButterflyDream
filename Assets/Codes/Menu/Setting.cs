25bdf9471bde78579b3a61207e8115cd8e7fe565
yuchuliu <yuchuliu@usc.edu>
2020-10-05 23:33:11 -0700

    changing volume

---
+++ Assets/Codes/Menu/Setting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Setting : MonoBehaviour
+{
+	public AudioMixer MainVolumeMixer;
+    public void SetVolume(float Volume) {
+    	MainVolumeMixer.SetFloat("MainVolumeMixer", Volume);
+    }
+}
