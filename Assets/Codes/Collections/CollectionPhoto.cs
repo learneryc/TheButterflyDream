690b2ca017011e8813eab106ca73abd5f8c37a5e
new-yongsheng <yongshen@usc.edu>
2020-10-12 17:02:00 -0700

    Add trasition scene and chage jack's image

---
+++ Assets/Codes/Collections/CollectionPhoto.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionPhoto : CollectionsAction
+{
+    private Fungus.Flowchart fc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        fc = GameObject.Find("Flowchart").GetComponent<Fungus.Flowchart>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(pickUpAllowed && Input.GetKeyDown(KeyCode.E)){
+            PlayerPrefs.SetString("SceneName", "LevelThree");
+            fc.ExecuteBlock("Photo");
+        	PickUp();
+        }
+        
+    }
+
+    void PickUp(){
+        
+            /*GameObject.Find("LevelLoader").
+            GetComponent<LevelLoader>().MoveToNextLevel();
+            PersistentData.update();*/
+        AudioManager.instance.Play("Sound/pickup");
+    	Destroy(gameObject);
+    }
+}
