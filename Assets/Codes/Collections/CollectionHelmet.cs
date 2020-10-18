46c94973038ced352924dfd04415309910b9f927
Ym111 <ym_chen95@outlook.com>
2020-10-18 15:25:06 -0700

    organise the folder

---
+++ Assets/Codes/Collections/CollectionHelmet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionHelmet : CollectionsAction
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pickUpAllowed && Input.GetKeyDown(KeyCode.E)){
+        	PickUp();
+        }
+    }
+}
