c3fe2033b5548e9b7cbff208417b3f9173450603
new-yongsheng <yongshen@usc.edu>
2020-10-19 13:50:50 -0700

    Compelete a npc dialog

---
+++ Assets/Codes/Character/NPCControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class NPCControl : MonoBehaviour
+{
+    public string ChatName;
+    private bool canChat = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other){
+        canChat = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D other){
+        canChat = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)){
+            Say();
+        }
+    }
+
+    void Say(){
+        if(canChat){
+            Flowchart flowChart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+            flowChart.ExecuteBlock(ChatName);
+        }
+    }
+}
