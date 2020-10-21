733cc4eb53abc4dc872966562e60c70e85380f31
Ym111 <ym_chen95@outlook.com>
2020-10-20 20:07:53 -0700

    add hint for enter door and fix bugs flash between two door

--- Assets/Codes/SceneTransition/DoorEnter.cs
+++ Assets/Codes/SceneTransition/DoorEnter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorEnter : MonoBehaviour
 {
@@ -10,16 +11,19 @@ public class DoorEnter : MonoBehaviour
 
     private bool isDoor;
 
+    public Text enterDoorText;
+
     // Start is called before the first frame update
     void Start()
     {
+        enterDoorText.gameObject.SetActive(false);
         playerTransform = GameObject.Find("Jack").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.I) && isDoor) {
+        if (Input.GetKeyDown(KeyCode.I) && isDoor) {
             Vector3 pos = playerTransform.position;
             pos.x = backDoor.position.x;
             pos.y = backDoor.position.y - 0.8f;
@@ -32,12 +36,14 @@ public class DoorEnter : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             isDoor = true;
+            enterDoorText.gameObject.SetActive(true);
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             isDoor = false;
+            enterDoorText.gameObject.SetActive(false);
         }
     }
 }
