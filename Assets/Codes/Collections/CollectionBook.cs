16d6abc845c7f40194599a0d184835ba2c8776a3
Ym111 <ym_chen95@outlook.com>
2020-10-22 19:19:27 -0700

    add magic book to learn magic

---
+++ Assets/Codes/Collections/CollectionBook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionBook : CollectionsAction
+{
+    public Button magic2 ; 
+    // Start is called before the first frame update
+    void Start()
+    {
+         base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pickUpAllowed && Input.GetKeyDown(KeyCode.E)){
+            PersistentData.setMagic2(1);
+            magic2.gameObject.SetActive(true);
+            AudioManager.instance.Play("Sound/pickup");
+    	    Destroy(gameObject);
+        }
+    }
+ 
+}
