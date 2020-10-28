d0ad293487f32d4c4a34d5a1f761d6ea4bad4e0a
Ym111 <ym_chen95@outlook.com>
2020-10-27 20:22:02 -0700

    Fix level three bugs and delect duo bind of magic and playerMagic

--- Assets/Codes/Character/PlayerMagicAttack.cs
+++ Assets/Codes/Character/PlayerMagicAttack.cs
@@ -15,9 +15,7 @@ public class PlayerMagicAttack : MonoBehaviour
     public float magic2CDTime = 1f;
     private bool magic3Allowed = true;
     public float magic3CDTime = 10f;
-    private MagicCDIcon1 icon1;
-    private MagicCDIcon2 icon2;
-    private MagicCDIcon3 icon3;
+
     // magic2
     private Animator anim;
     
@@ -28,25 +26,12 @@ public class PlayerMagicAttack : MonoBehaviour
     void Start()
     {
         player = GetComponent<Swordman>();
-        icon1 = GameObject.Find("Magic1Button").GetComponent<MagicCDIcon1>();
-        icon2 = GameObject.Find("Magic2Button").GetComponent<MagicCDIcon2>();
-        icon3 = GameObject.Find("Magic3Button").GetComponent<MagicCDIcon3>();
         anim = this.transform.Find("model").GetComponent<Animator>();
     }
 
     // Update is called once per frame
      void Update()
      {
-        // if (Input.GetKeyDown(KeyCode.R)&&magic1Allowed)
-        // {
-        //         FireBallAttack();
-        //         icon1.StartMagic();
-        // }
-        // if (Input.GetKeyDown(KeyCode.T)&&magic2Allowed)
-        // {
-        //         magic2Magic();
-        //         icon2.StartMagic();
-        // }
      }
      //Magic 1
      public void FireBallAttack()
@@ -60,6 +45,8 @@ public class PlayerMagicAttack : MonoBehaviour
          yield return new WaitForSeconds(magic1CDTime);
          magic1Allowed = true;
      }
+
+
      //Magic 2 -> double the damage but also double the hurt 
      public void magic2Magic()
      {
@@ -73,6 +60,9 @@ public class PlayerMagicAttack : MonoBehaviour
          yield return new WaitForSeconds(magic2CDTime);
          magic2Allowed = true;
      }
+
+
+     
      //Magic 3
      public void magic3Magic()
      {
