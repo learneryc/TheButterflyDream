00932aebce91916aa2e8670b5064c2bac0b26f32
yuchuliu <yuchuliu@usc.edu>
2020-12-01 23:23:59 -0800

    add new pause features

---
+++ Assets/Codes/Icons/ClickAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAnimation : MonoBehaviour
+{
+	public float scale = -0.1f;
+	public int size = 10;
+	private Vector3[] scaleChange;
+	private int sIterator;
+
+	void Start() {
+		float factor = 2*scale/size;
+		scaleChange = new Vector3[size];
+		Vector3 change = new Vector3(factor,factor,0f);
+		for (int i=0;i<size/2;i++) {
+			scaleChange[i] = change;
+		}
+		for (int i=(size+1)/2;i<size;i++) {
+			scaleChange[i] = -change;
+		}
+		sIterator = size;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (sIterator == size) return;
+        this.transform.localScale += scaleChange[sIterator];
+        sIterator++;
+    }
+
+    public void Click() {
+    	sIterator = 0;
+    }
+}
--- Assets/Codes/Menu/PauseMenu.cs
+++ Assets/Codes/Menu/PauseMenu.cs
@@ -5,31 +5,79 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
 	public GameObject pauseMenuUI;
+    public GameObject optionMenu;
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
 	public float PauseButtonLength = 0.2f;
+    public int clickFrames = 10;
+
+    private bool resume = false;
+    private bool option = false;
+    private bool back  = false;
+    private int frameCounter = 0;
+
+    void Update() {
+        if (resume) {
+            if (frameCounter == clickFrames) {
+                resume = false;
+                Time.timeScale = 1f;
+                pauseMenuUI.SetActive(false);
+            }
+            frameCounter++;
+        }
+
+        if (option) {
+            if (frameCounter == clickFrames) {
+                option = false;
+                pauseMenu.SetActive(false);
+                optionMenu.SetActive(true);
+            }
+            frameCounter++;
+        }
+
+        if (back) {
+            if (frameCounter == clickFrames) {
+                back = false;
+                optionMenu.SetActive(false);
+                pauseMenu.SetActive(true);
+            }
+            frameCounter++;
+        }
+
+
+    }
 
 	public void Pause() {
-		StartCoroutine(LoadAnimation(PauseButtonLength, true, 0f));
+		StartCoroutine(LoadAnimation(pauseMenuUI, PauseButtonLength, true, 0f));
 	}
 
 	public void Resume() {
-		pauseMenuUI.SetActive(false);
-		Time.timeScale = 1f;
+        resume = true;
+        frameCounter = 0;
 	}
+
+    public void Option() {
+        option = true;
+        frameCounter = 0;
+    }
+
+    public void Back() {
+        back = true;
+        frameCounter = 0;
+    }
 
     public void Menu() {
     	Time.timeScale = 1f;
+        pauseButton.SetActive(false);
     	GameObject.Find("LevelLoader").
             GetComponent<LevelLoader>().MoveToNextLevel("StartMenu");
     }
 
-    public void Quit() {
-    	Application.Quit();
-    }
 
-    IEnumerator LoadAnimation(float length=0.2f, bool isActive=true, float scale=0)
+    IEnumerator LoadAnimation(GameObject menu, float length=0.2f, bool isActive=true, float scale=0)
     {
     	yield return new WaitForSeconds(length);
-    	pauseMenuUI.SetActive(isActive);
+    	menu.SetActive(isActive);
 		Time.timeScale = scale;
     }
 }
